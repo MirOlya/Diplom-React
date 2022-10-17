37ddc2b6dc877a8a66ddb936615e6310cf725b60
Ольга Миронович <ip.mironovich@gmail.com>
2022-10-17 16:12:04 +0300

    11

---
+++ v1ts/App.js
@@ -0,0 +1,17 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import PagesRouter from './src/pages/PagesRouter';
+import PagesLinks from './src/pages/PagesLinks';
+
+ReactDOM.render( 
+  <BrowserRouter>
+    <div>
+      <PagesLinks />
+      <PagesRouter />
+    </div>
+  </BrowserRouter>
+, document.getElementById('container') );
